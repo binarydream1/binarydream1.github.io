28b7c1f4bba8d2dbe9eb060f7737faa3e07425f7
Binary-Dream <47627429+binarydream1@users.noreply.github.com>
2019-03-27 22:38:21 +0700

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,54 +1,23 @@
-var ws = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=1089');
-var token = document.getElementById("myToken").value;
-ws.onopen = function(evt) 
-{
-  ws.send(JSON.stringify({authorize:'token'}));
-  ws.send(JSON.stringify({
-  proposal: 1,
-  amount: "10",
-  basis: "stake",
-  contract_type: "CALL",
-  currency: "USD",
-  duration: "60",
-  duration_unit: "s",
-  barrier: "+0.1",
-  symbol: "R_10",
-  subscribe:1}));
-  
-  ws.send(JSON.stringify({
-  proposal: 1,
-  amount: "10",
-  basis: "stake",
-  contract_type: "CALL",
-  currency: "USD",
-  duration: "60",
-  duration_unit: "s",
-  barrier: "+0.1",
-  symbol: "R_25",
-  subscribe:1}));
-  
-};
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
 
-ws.onmessage = function(msg) 
-{
-   var data = JSON.parse(msg.data);
-   console.log(data);
-   var dtkEpoch = data.proposal.spot_time;
-   var myDate = new Date( dtkEpoch *1000);
-   
-   
-   document.getElementById("waktu").innerHTML =myDate.toUTCString()+" -- (Local Time: "+myDate.toLocaleTimeString()+")";
-  
-  var aset = data.echo_req.symbol;
-  
-  if(aset === "R_10"){
-   
-     document.getElementById("aset r10").innerHTML ="Volatility Index "+ data.echo_req.symbol;
-     document.getElementById("spot").innerHTML =data.proposal.spot;
-}else{
-     document.getElementById("aset r25").innerHTML ="Volatility Index "+ data.echo_req.symbol;
-     document.getElementById("spot2").innerHTML =data.proposal.spot;
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
 }
-  
-  
- };
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
